08413bc95f3958c12abcd130ac9540dcd3920b10
Vincent Kuan <vince_k372@yahoo.com>
2023-02-26 15:48:52 -0500

    app on root

---
+++ App.js
@@ -0,0 +1,12 @@
+import './kanbanboard.css';
+import SortableList from "./component/SortableList";
+
+function App() {
+  return (
+    <div className="container">
+      <SortableList />
+    </div>
+  );
+}
+
+export default App;
